a17b7232c5917b916dac6b1c0c29364f1b11855d
NONHOUEGNON <nonhouegnonoroncer@gmail.com>
2026-05-24 19:30:04 +0100

    Ajout de .onrender.com à ALLOWED_HOSTS

--- fermeapp/settings.py
+++ fermeapp/settings.py
@@ -18,7 +18,11 @@ SECRET_KEY = 'django-insecure-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',  # accepte tous les sous-domaines onrender.com
+]
 
 
 # Application definition

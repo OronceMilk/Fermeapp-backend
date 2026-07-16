b37dc18ab1c95e683b8341a51d54d706a961f760
NONHOUEGNON <nonhouegnonoroncer@gmail.com>
2026-07-16 21:33:55 +0100

    Use StaticFilesStorage temporarily to avoid post-processing errors during collectstatic

--- fermeapp/settings.py
+++ fermeapp/settings.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # ============================================
 # DEFAULT PRIMARY KEY

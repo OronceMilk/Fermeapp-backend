3ce05dc27ee39b0da02c8c9ac3b867dbbdf25274
NONHOUEGNON <nonhouegnonoroncer@gmail.com>
2026-07-16 20:36:48 +0100

    Fix: Nettoyage forcé de staticfiles avant collectstatic

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-#!/bin/bash 
-pip install -r requirements.txt 
-python manage.py migrate 
-python manage.py collectstatic --noinput 
+#!/bin/bash
+echo "🗑️ Suppression de l'ancien dossier staticfiles..."
+rm -rf staticfiles
+
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput

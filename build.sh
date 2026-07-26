31dd64379ef1bf6beca685f3ad6cbd778a92ae99
NONHOUEGNON <nonhouegnonoroncer@gmail.com>
2026-07-26 21:01:09 +0100

    Fix: build.sh avec copie de secours multi-source (projet, admin, cloudinary)

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "🗑️ Suppression de l'ancien dossier staticfiles..."
-rm -rf staticfiles
-
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
